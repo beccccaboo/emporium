ea6b7f565fb41a7a6e831d2a476df68360be4fce
Rebecca Biju <biju.r@northeastern.edu>
2022-12-01 11:39:17 -0500

    Ecosystem and DB4O setup

---
+++ src/business/EcoSystem.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package business;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author rebeccabiju
+ */
+public class EcoSystem {
+    private static EcoSystem ecoSystem;
+    private ArrayList<Network> networkList;
+
+    private EcoSystem() {
+        super(null);
+        networkList = new ArrayList();
+    }
+
+    public static EcoSystem getInstance() {
+        if (ecoSystem == null) {
+            ecoSystem = new EcoSystem();
+        }
+        return ecoSystem;
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new SystemAdminRole());
+        return roles;
+    }
+
+    public ArrayList<Network> getNetworkList() {
+        return networkList;
+    }
+
+    public void setNetworkList(ArrayList<Network> networkList) {
+        this.networkList = networkList;
+    }
+
+    public Network createAndAddNetwork() {
+        Network n = new Network();
+        networkList.add(n);
+        return n;
+    }
+
+    public boolean checkIfNetworkExisits(String networkName) {
+        for (Network n : networkList) {
+            if (n.getName().equals(networkName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkIfUserNameIsUnique(String userName) {
+        for (Network n : networkList) {
+            for (Enterprise e : n.getEnterpriseDirectory().getEnterpriseList()) {
+
+                // Check Enterprise's UAD
+                UserAccountDirectory uad = e.getUserAccountDirectory();
+                if (uad.checkIfUserNameExists(userName)) {
+                    return true;
+                }
+
+                // Check Enterprise's organizations
+                for (Organization o : e.getOrganizationDirectory().getOrganizationList()) {
+                    uad = o.getUserAccountDirectory();
+                    if (uad.checkIfUserNameExists(userName)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
