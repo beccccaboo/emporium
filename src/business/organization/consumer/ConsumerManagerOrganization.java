7980a978f5339708e9f09713513e9e6e2c30f9f6
Swarag Gutte <gutte.s@northeastern.edu>
2022-12-02 18:42:42 -0500

    Added manager class

--- src/business/organization/consumer/ConsumerManagerOrganization.java
+++ src/business/organization/consumer/ConsumerManagerOrganization.java
@@ -3,7 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package business.organization.consumer;
-
+import business.organization.Organization;
+import business.role.Role;
+import business.role.consumer.ConsumerManagerRole;
+import java.util.ArrayList;
 /**
  *
  * @author swarag
