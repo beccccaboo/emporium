a713f384a6323d97edc832ab4a95350b7eaf1754
Swarag Gutte <gutte.s@northeastern.edu>
2022-12-02 18:21:26 -0500

    Added imports to consumer worker class

--- src/business/role/consumer/ConsumerWorkerRole.java
+++ src/business/role/consumer/ConsumerWorkerRole.java
@@ -3,6 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package business.role.consumer;
+import business.EcoSystem;
+import business.enterprise.Enterprise;
+import business.network.Network;
+import business.organization.Organization;
+import business.role.Role;
+import business.userAccount.UserAccount;
+import javax.swing.JPanel;
+import userInterface.ngo.ngoWorker.NGOWorkerWorkAreaJPanel;
 
 /**
  *
