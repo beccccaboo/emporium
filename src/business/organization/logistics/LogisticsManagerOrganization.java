ff736f8e2fd1d25ecd4e2a8cec32d0da05e077e8
Arpit Jain <jain.arpit@northeastern.edu>
2022-12-01 21:49:56 -0500

    LogisticsManagerOrganization

---
+++ src/business/organization/logistics/LogisticsManagerOrganization.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package business.organization.logistics;
+
+/**
+ *
+ * @author Arpit
+ */
+public class LogisticsManagerOrganization {
+    
+}
