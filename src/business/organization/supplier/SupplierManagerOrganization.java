7071251c9d7646dff68f90765bd21091ae8e405e
Rebecca Biju <biju.r@northeastern.edu>
2022-12-01 22:02:35 -0500

    Define Supplier Manager Organization

---
+++ src/business/organization/supplier/SupplierManagerOrganization.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package business.organization.supplier;
+
+import business.organization.Organization;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author rebeccabiju
+ */
+public class SupplierManagerOrganization extends Organization {
+
+    public SupplierManagerOrganization() {
+        super(Organization.Type.SupplierManager.getValue());
+    }
+
+    @Override
+    public ArrayList<Role> getSupportedRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new SupplierManagerOrganizationRole());
+        return roles;
+    }
+
+}
