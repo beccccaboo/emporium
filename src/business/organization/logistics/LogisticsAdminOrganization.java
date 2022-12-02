b551ac2fb77728775c6c934635ddd21da5417d5d
Arpit Jain <jain.arpit@northeastern.edu>
2022-12-01 21:43:33 -0500

    Logistics

---
+++ src/business/organization/logistics/LogisticsAdminOrganization.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+ 
+ 
+package business.organization.logistics;
+
+import business.organization.Organization;
+import business.role.Role;
+import business.role.logistics.LogisticsAdminRole;
+import java.util.ArrayList;
+/**
+ *
+ * @author Arpit
+ */
+public class LogisticsAdminOrganization extends Organization{
+    public LogisticsAdminOrganization() {
+        super(Type.LogisticsAdmin.getValue());
+    }
+    
+}
