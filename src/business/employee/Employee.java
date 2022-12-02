bbb742fe9779e1a3bd64a5fcb5975fe9c0cac53f
Rebecca Biju <biju.r@northeastern.edu>
2022-12-01 22:43:44 -0500

    Define Employee Directory

--- src/business/employee/Employee.java
+++ src/business/employee/Employee.java
@@ -9,5 +9,28 @@ package business.employee;
  * @author rebeccabiju
  */
 public class Employee {
+    private static int count = 0;
+    private int id;
+    private String name;
+
+    public Employee() {
+        id = ++count;
+    }
+
+    public int getId() {
+        return id;
+    }
     
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    @Override
+    public String toString(){
+        return name;
+    }
 }
